1aad619cd87c444feedcb5392fd5c088811d2610
Anais <a6anaistang@hotmail.co.uk>
2020-07-04 17:52:19 +0100

    nucleod dna completed

--- dnastring.py
+++ dnastring.py
@@ -3,12 +3,25 @@ A string is simply an ordered collection of symbols selected from some alphabet 
 An example of a length 21 DNA string (whose alphabet contains the symbols 'A', 'C', 'G', and 'T') is "ATGCTTCAGAAAGGTCTTACG."
 Given: A DNA string s of length at most 1000 nt.
 Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+
 Sample Dataset:
 AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
 
 Sample Output:
-
 20 12 17 21
 
-NOTE -> Must be in class and method format
 """
+
+
+
+sample_dna = "AGCGTGTGGATTAATTACGGGTTCATTCTGACTAGCACAATATGTCTCTAATGATAGCAGCGTGT"
+
+class Nucleod:
+
+    def count_dna():
+        n = input("Please enter your DNA data:\n")
+        print("A: " + str(n.count("A")),"C: "+ str(n.count("C")), "G: " + str(n.count("G")), "T: " + str(n.count("T")))
+
+
+
+Nucleod.count_dna()
