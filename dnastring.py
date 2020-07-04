06c0d98200d3d1e2e66dc4168df6903f080a4648
Anais <a6anaistang@hotmail.co.uk>
2020-07-04 16:25:32 +0100

    scrabble

---
+++ dnastring.py
@@ -0,0 +1,14 @@
+"""
+A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length of a string is the number of symbols that it contains.
+An example of a length 21 DNA string (whose alphabet contains the symbols 'A', 'C', 'G', and 'T') is "ATGCTTCAGAAAGGTCTTACG."
+Given: A DNA string s of length at most 1000 nt.
+Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+Sample Dataset:
+AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
+
+Sample Output:
+
+20 12 17 21
+
+NOTE -> Must be in class and method format
+"""
