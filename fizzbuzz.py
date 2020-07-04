d68f2ab570b9420d6f3b620c7952cb76c74f75f2
Anais <a6anaistang@hotmail.co.uk>
2020-07-04 15:26:55 +0100

    fizbuzz fixed error

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -10,22 +10,20 @@ NOTE -> Must be in class and method format
 class Fizzy:
 
     def fizzbuzz():
-        while True:
             try:
                 n = int(input("Please select a number between 1 and 100: \n"))
             except ValueError:
                 n = int(input("The input was not a valid integer or out of range. Please select a number between 1 and 100: \n"))
             if n % 3 == 0 and n % 5 == 0:
-                return f"The you have selected is: {n}\nThe outcome is: fizzbuzz"
+                return f"The number you have selected is: {n}\nThe outcome is: fizzbuzz"
             elif n % 3 == 0:
-                return f"The you have selected is: {n}\nThe outcome is: fizz"
+                return f"The number you have selected is: {n}\nThe outcome is: fizz"
             elif n % 5 == 0:
                 return f"The number you have selected is: {n}\nThe outcome is: buzz"
             else:
                 return f"The number you have selected is: {n}\nIt is neither divisible by 3 nor 5"
-
     print(fizzbuzz())
 
 Fizzy()
-Fizzy.fizzbuzz()
+
 
