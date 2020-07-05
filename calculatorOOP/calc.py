24f6ed1673ecec0c52c57d2984397bdb00579289
Anais <a6anaistang@hotmail.co.uk>
2020-07-05 22:37:45 +0100

    added cm to inch converter

--- calculatorOOP/calc.py
+++ calculatorOOP/calc.py
@@ -1,4 +1,5 @@
-from calculatorOOP.calculator import *
+from calculator import *
+
 
 Calc = Simple_Calculator(num1, num2)
 
@@ -6,13 +7,17 @@ while True:
     def menu():
         options = """
         Please select one of the following: 
-            ==================================
+            =======================================
             + for addition
             - for subtraction 
             * for multiplication 
             / for division
-            0 to Exit
-            ==================================
+            % for modulus
+            tr to find the area of a triangle
+            in to convert cm_to_inches 
+            cm to convert inches_to_cm 
+            0 to exit
+            ========================================
             """
         print(options)
     menu()
@@ -25,6 +30,14 @@ while True:
         print(Calc.multiply())
     elif user_input == "/":
         print(Calc.divide())
+    elif user_input == "%":
+        print(Calc.modulo())
+    elif user_input == "tr":
+        print(Calc.area_triangle())
+    elif user_input == "cm":
+        print(Calc.cm_to_inches())
+    elif user_input == "in":
+        print(Calc.inches_to_cm())
     elif user_input == "0":
         print("Goodbye! See you again soon!")
         break
--- calculatorOOP/calculator.py
+++ calculatorOOP/calculator.py
@@ -10,6 +10,17 @@ class Simple_Calculator():
         return f"{self.num1} * {self.num2} = {self.num1 * self.num2}"
     def divide(self):
         return f"{self.num1}/{self.num2} = {self.num1 / self.num2}"
+    def modulo(self):
+        return f"{self.num1}%{self.num2} = {self.num1%self.num2}"
+
+    def area_triangle(self):
+        return f"The area of triangle is = {(self.num1/0.5)* self.num2}"
+
+    def cm_to_inches(self):
+        return f"{self.num1}cm to inches is = {self.num1/2.54}\n{self.num2}cm to inches is = {self.num2/2.54}"
+    
+    def inches_to_cm(self):
+        return f"{self.num1} inches to cm is = {self.num1*2.54}\n{self.num2} inches to cm is ={self.num2*2.54}"
 
 num1 = int(input("Enter First Number: "))
 num2 = int(input("Enter Second Number: "))
